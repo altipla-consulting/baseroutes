4265cf739a980ecbdc111bc4501d74e20cfe429b
Ernesto Alejo <ernestokarim@gmail.com>
2022-12-15 11:06:54 +0100

    Add precomposed apple icon to the routes.

--- baseroutes.go
+++ baseroutes.go
@@ -29,6 +29,7 @@ func Register(r *routing.Router, baseTemplate string, options ...RegisterOption)
 	r.Get("/robots.txt", fileHandler("robots.txt"))
 	r.Get("/favicon.ico", fileHandler("favicon.ico"))
 	r.Get("/apple-touch-icon.png", fileHandler("apple-touch-icon.png"))
+	r.Get("/apple-touch-icon-precomposed.png", fileHandler("apple-touch-icon.png"))
 
 	for _, opt := range options {
 		opt(r)
